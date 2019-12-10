879ff945d5a12cf02fe523a67386c96d0325f437
李观保 <sma2lbao@qq.com>
2019-12-10 11:36:59 +0800

    播放页修修补补

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -5,35 +5,50 @@ import NextPlay from "./components/NextPlay";
 import VideoInfo from "./components/VideoInfo";
 import Comment from "./components/Comment";
 import { Box } from "@material-ui/core";
-import { MediaType } from "./components/VideoPlayer/player.interface";
+import {
+  FullScreen,
+  BodyScreen,
+  ContentScreen,
+  AiderScreen
+} from "@/layouts/PageLayout";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      backgroundColor: theme.palette.background.paper,
       height: "100%"
+    },
+    body: {
+      backgroundColor: theme.palette.background.paper,
+      display: "flex",
+      margin: "0 auto"
+    },
+    content: {
+      padding: theme.spacing(2, 5),
+      borderRight: `1px solid ${theme.palette.divider}`
+    },
+    aider: {
+      padding: theme.spacing(2, 4)
     }
   })
 );
 
 export default function Player() {
   const classes = useStyles();
-  // https://player.vimeo.com/external/188421287.sd.mp4?s=bdbf8a61c40502211971571fef384f52fe79dbbe&profile_id=164
-  // https://pan.baidu.com/api/streaming?path=%2F%E6%88%91%E7%9A%84%E8%B5%84%E6%BA%90%2Fflutter%E5%9F%BA%E7%A1%80-%20%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B%20%E4%B8%AD%E6%96%87%E5%AD%97%E5%B9%95%2F12.Navigation.mp4&app_id=250528&clienttype=0&type=M3U8_FLV_264_480&vip=0&adToken=
 
   return (
     <Box className={classes.root}>
-      <VideoPlayer
-        mediaType={MediaType.M3U8}
-        src="https://pan.baidu.com/api/streaming?path=%2F%E6%88%91%E7%9A%84%E8%B5%84%E6%BA%90%2Fflutter%E5%9F%BA%E7%A1%80-%20%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B%20%E4%B8%AD%E6%96%87%E5%AD%97%E5%B9%95%2F12.Navigation.mp4&app_id=250528&clienttype=0&type=M3U8_FLV_264_480&vip=0&adToken="
-      />
-      {/* <div>
-        <VideoInfo />
-      </div>
-      <div>
-        <NextPlay />
-      </div> */}
-      {/* <Comment /> */}
+      <FullScreen>
+        {/* <VideoPlayer src="https://player.vimeo.com/external/188421287.sd.mp4?s=bdbf8a61c40502211971571fef384f52fe79dbbe&profile_id=164" /> */}
+      </FullScreen>
+      <BodyScreen className={classes.body}>
+        <ContentScreen className={classes.content}>
+          <VideoInfo />
+          <NextPlay />
+        </ContentScreen>
+        <AiderScreen className={classes.aider}>
+          <Comment />
+        </AiderScreen>
+      </BodyScreen>
     </Box>
   );
 }
