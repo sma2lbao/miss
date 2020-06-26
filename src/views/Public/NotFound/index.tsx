9327c466612f4826b3c171e4a3bf4dfab5a4ee12
sma2lbao <sma2lbao@qq.com>
2020-06-26 18:14:50 +0800

    404 page change.

--- src/views/Public/NotFound/index.tsx
+++ src/views/Public/NotFound/index.tsx
@@ -1,5 +1,49 @@
 import * as React from "react";
+import { FullScreen, BodyScreen } from "@/layouts/PageLayout";
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+  Box,
+  Typography,
+  Button
+} from "@material-ui/core";
+import clsx from "clsx";
 
-export default function NotFound() {
-  return <div>页面未找到</div>;
-}
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      overflowY: "auto"
+    },
+    box: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    icon: {
+      fontSize: 120
+    }
+  })
+);
+
+export const NotFound: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <FullScreen className={classes.root}>
+      <Box className={classes.box}>
+        <i className={clsx("iconfont icon-404", classes.icon)}></i>
+        <Typography variant="h2">Oops</Typography>
+        <Typography variant="body1">页面未找到！</Typography>
+        <Button>返回首页</Button>
+      </Box>
+    </FullScreen>
+  );
+};
+
+export default NotFound;
