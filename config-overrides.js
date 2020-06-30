5ca3fcbfc1306b5a6ef4f37838aabbd731c9a98f
李观保 <sma2lbao@qq.com>
2020-06-30 16:34:23 +0800

    gql配置问题

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    webpack: function(config) {
-        config.resolve = {
-            ...config.resolve,
-            alias: { '@': path.resolve(__dirname, 'src') },
-          };
-        return config
-    }
-}
+  webpack: function(config) {
+    // config.module.rules.push({
+    //   test: /\.(graphql|gql)$/,
+    //   exclude: /node_modules/,
+    //   loader: "graphql-tag/loader",
+    // });
+    config.resolve = {
+      ...config.resolve,
+      alias: { "@": path.resolve(__dirname, "src") },
+    };
+    return config;
+  },
+};
