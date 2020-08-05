8bbe759f902829a0c86b99fecb93f85b969b5b37
李观保 <sma2lbao@qq.com>
2020-08-05 20:00:05 +0800

    thirdPlatform auth.

--- src/views/auth/Login/modules/SignIn.tsx
+++ src/views/auth/Login/modules/SignIn.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button, IconButton } from "@material-ui/core";
 import { useSnackbar } from "notistack";
 // import { GitHub } from "@material-ui/icons";
-import { useLoginMutation } from "@/schema";
+import { useLoginMutation, usePlatformAuthWayQuery } from "@/schema";
 import { useRouterHelper } from "@/hooks";
+import { GitHub } from "@material-ui/icons";
 
 export default function SignIn() {
   const RouterHelper = useRouterHelper();
 
+  const { data: queryData, loading: queryLoading } = usePlatformAuthWayQuery();
+  console.log(queryData, queryLoading);
   const { enqueueSnackbar } = useSnackbar();
   const [login, res] = useLoginMutation({
     onCompleted(data) {
@@ -28,42 +31,37 @@ export default function SignIn() {
     login({ variables: { username, password } });
   };
 
-  // const github_login = function() {
-  //   const child = window.open(
-  //     "http://localhost:108/auth/github",
-  //     "github",
-  //     `width=450,height=450,toolbar=0,menubar=0,location=0,status=0`
-  //   );
-  //   if (child) {
-  //     const timer = setInterval(() => {
-  //       child.postMessage(
-  //         {
-  //           type: "access_token",
-  //           signal: "天王盖地虎",
-  //         },
-  //         "http://localhost:108"
-  //       );
-  //     }, 1000);
+  const thirdLogin = function() {
+    const child = window.open(
+      "http://localhost:108/auth/github",
+      "github",
+      `width=450,height=450,toolbar=0,menubar=0,location=0,status=0`
+    );
+    if (child) {
+      const timer = setInterval(() => {
+        child.postMessage(
+          {
+            type: "access_token"
+          },
+          "http://localhost:108"
+        );
+      }, 1000);
 
-  //     window.addEventListener("message", (event) => {
-  //       const data = event.data;
-  //       if (
-  //         data &&
-  //         data.type === "access_token_callback" &&
-  //         data.signal === "宝塔镇河妖"
-  //       ) {
-  //         clearInterval(timer);
-  //         child && child.close();
+      window.addEventListener("message", event => {
+        const data = event.data;
+        if (data && data.type === "access_token_callback") {
+          clearInterval(timer);
+          child && child.close();
 
-  //         enqueueSnackbar(
-  //           data.data.access_token ? "登录成功" : data.data.error || "登录失败"
-  //         );
-  //         localStorage.setItem("access_token", data.data.access_token);
-  //         history.push("/home");
-  //       }
-  //     });
-  //   }
-  // };
+          enqueueSnackbar(
+            data.data.access_token ? "登录成功" : data.data.error || "登录失败"
+          );
+          localStorage.setItem("access_token", data.data.access_token);
+          // history.push("/home");
+        }
+      });
+    }
+  };
 
   return (
     <div>
@@ -91,7 +89,16 @@ export default function SignIn() {
           {loading ? "登录中" : "登录"}
         </Button>
       </form>
-      {/* <IconButton onClick={github_login}>
+      {queryData?.platform_auth_way.map((item, idx) => {
+        return (
+          <div key={idx}>
+            <IconButton onClick={thirdLogin}>
+              <GitHub />
+            </IconButton>
+          </div>
+        );
+      })}
+      {/* <IconButton >
         <GitHub />
       </IconButton> */}
     </div>
