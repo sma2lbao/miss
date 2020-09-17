b71db7bde7de029e61b44b934f384c0ab0324573
sma2lbao <sma2lbao@qq.com>
2020-09-17 18:32:12 +0800

    apollo client update.

--- src/hooks/auth.helper.ts
+++ src/hooks/auth.helper.ts
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { useMeLazyQuery, User } from "@/schema";
 import { useRouterHelper } from "./router.helper";
-// import { accessTokenVar } from "@/graphql/variables";
+import { useReactiveVar } from "@apollo/client";
+import { accessTokenVar } from "@/graphql/variables";
 
 export const useAuth = () => {
   const RouterHelper = useRouterHelper();
   const [hasLogged, setHasLogged] = React.useState<boolean>(false);
   const [member, setMember] = React.useState<User | null>(null);
   const [meQuery, { data }] = useMeLazyQuery();
+  const access_token = useReactiveVar(accessTokenVar);
 
   React.useEffect(() => {
     // if (access_token()) {
