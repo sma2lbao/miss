de72c9075947c17ebb253cd7603a12412e8737e2
sma2lbao <sma2lbao@qq.com>
2020-10-27 18:31:00 +0800

    account login ux.

--- src/layouts/Main/modules/Topbar/components/Account.tsx
+++ src/layouts/Main/modules/Topbar/components/Account.tsx
@@ -1,35 +1,78 @@
 import * as React from "react";
 import { AccountCircle } from "@material-ui/icons";
-import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import {
+  Menu,
+  MenuItem,
+  Button,
+  Avatar,
+  makeStyles,
+  Theme,
+  createStyles,
+  Box
+} from "@material-ui/core";
+import { useAuth, useRouterHelper } from "@/hooks";
 
-export default function Account() {
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    avatar: {
+      width: theme.spacing(4.5),
+      height: theme.spacing(4.5)
+    }
+  });
+});
+
+export const Account: React.FC = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const { hasLogged, member } = useAuth();
+  const RouterHelper = useRouterHelper();
+  const classes = useStyles();
 
   function _close() {
     setAnchorEl(null);
   }
 
+  const handleLogin = () => {
+    RouterHelper.redirectAuthLogin();
+  };
+
   return (
-    <>
-      <IconButton
-        aria-controls="account-menu"
-        aria-haspopup="true"
-        onClick={event => {
-          setAnchorEl(event.currentTarget);
-        }}
-        color="inherit"
-      >
-        <AccountCircle />
-      </IconButton>
-      <Menu
-        id="account-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={_close}
-      >
-        <MenuItem onClick={_close}>Resume</MenuItem>
-      </Menu>
-    </>
+    <Box mx={0.5}>
+      {hasLogged ? (
+        <>
+          <div
+            onClick={event => {
+              setAnchorEl(event.currentTarget);
+            }}
+          >
+            {member?.avatar ? (
+              <Avatar
+                className={classes.avatar}
+                alt={member.username}
+                src={member.avatar}
+              ></Avatar>
+            ) : (
+              <Avatar className={classes.avatar} alt={member?.username}>
+                <AccountCircle />
+              </Avatar>
+            )}
+          </div>
+          <Menu
+            id="account-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={_close}
+          >
+            <MenuItem onClick={_close}>Resume</MenuItem>
+          </Menu>
+        </>
+      ) : (
+        <>
+          <Button onClick={handleLogin}>login</Button>
+        </>
+      )}
+    </Box>
   );
-}
+};
+
+export default Account;
--- src/layouts/Main/modules/Topbar/components/Notice.tsx
+++ src/layouts/Main/modules/Topbar/components/Notice.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Box } from "@material-ui/core";
 import { Mail } from "@material-ui/icons";
 
 export default function Notice() {
   return (
-    <IconButton color="inherit">
-      <Mail />
-    </IconButton>
+    <Box mx={0.5}>
+      <IconButton color="inherit">
+        <Mail />
+      </IconButton>
+    </Box>
   );
 }
