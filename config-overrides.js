7217f550907525872a74d892bd3ca2caaace1343
李观保 <sma2lbao@qq.com>
2020-11-25 16:14:15 +0800

    splitChunks overriide.

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@ const path = require("path");
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = {
-  webpack: function (config, env) {
+  webpack: function(config, env) {
     // config.module.rules.push({
     //   test: /\.(graphql|gql)$/,
     //   exclude: /node_modules/,
@@ -39,10 +39,26 @@ module.exports = {
       ]);
     }
 
+    config.optimization.splitChunks = {
+      ...config.optimization.splitChunks,
+      maxInitialRequests: Infinity,
+      cacheGroups: {
+        vendor: {
+          chunks: "all",
+          // minChunks: 1,
+          test: /[\\/]node_modules[\\/](react|react-dom|react-player|@material-ui|@apollo|graphql|@sentry|@firebase)[\\/]/,
+          name: (module) => {
+            return `npm.${
+              module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+            }`;
+          },
+        },
+      },
+    };
     return config;
   },
-  devServer: function (configFunction) {
-    return function (proxy, allowedHost) {
+  devServer: function(configFunction) {
+    return function(proxy, allowedHost) {
       // Create the default config by calling configFunction with the proxy/allowedHost parameters
       const config = configFunction(proxy, allowedHost);
       // Change the https certificate options to match your certificate, using the .env file to
@@ -58,7 +74,7 @@ module.exports = {
       return config;
     };
   },
-  paths: function (paths, env) {
+  paths: function(paths, env) {
     // ...add your paths config
     return paths;
   },
